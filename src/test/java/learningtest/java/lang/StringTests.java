89850ab56f88ad33006844301e66be13dd65db42
Johnny Lim <izeye@naver.com>
2024-03-05 14:24:36 +0900

    Add tests for String.split()

--- src/test/java/learningtest/java/lang/StringTests.java
+++ src/test/java/learningtest/java/lang/StringTests.java
@@ -20,4 +20,31 @@ class StringTests {
         assertThat(String.format(Locale.US, "%.1f", 1.25)).isEqualTo("1.3");
         assertThat(String.format(Locale.FRANCE, "%.1f", 1.25)).isEqualTo("1,3");
     }
+
+    @Test
+    void split() {
+        assertThat("a,b,c,d".split(","))
+                .hasSize(4)
+                .containsExactly("a", "b", "c", "d");
+
+        assertThat("a,b,c,d".split(",", 4))
+                .hasSize(4)
+                .containsExactly("a", "b", "c", "d");
+
+        assertThat("a,b,c,d".split(",", 3))
+                .hasSize(3)
+                .containsExactly("a", "b", "c,d");
+
+        assertThat("a,b,c,d".split(",", 5))
+                .hasSize(4)
+                .containsExactly("a", "b", "c", "d");
+
+        assertThat(",,,".split(","))
+                .isEmpty();
+
+        assertThat(",,,".split(",", 4))
+                .hasSize(4)
+                .allSatisfy((e) -> assertThat(e).isEmpty());
+    }
+
 }
