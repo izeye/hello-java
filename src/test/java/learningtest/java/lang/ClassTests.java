fc94b04a4229c9194394add9e2389ac59751f33f
Johnny Lim <izeye@naver.com>
2024-08-02 17:13:17 +0900

    Add tests for getMethods() and getDeclaredMethods() in Class

--- src/test/java/learningtest/java/lang/ClassTests.java
+++ src/test/java/learningtest/java/lang/ClassTests.java
@@ -2,6 +2,8 @@ package learningtest.java.lang;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -28,7 +30,36 @@ class ClassTests {
         assertThat(anonymousClassRunnable.getClass().getCanonicalName()).isNull();
     }
 
+    @Test
+    void getMethods() {
+        assertThat(Arrays.stream(Child.class.getMethods()).map((method) -> method.getName()))
+                .contains("getAlias")
+                .contains("getName")
+                .contains("toString");
+    }
+
+    @Test
+    void getDeclaredMethods() {
+        assertThat(Arrays.stream(Child.class.getDeclaredMethods()).map((method) -> method.getName())).containsExactly("getAlias");
+    }
+
     static class SomeClass {
+    }
+
+    public static class Parent {
+
+        public String getName() {
+            return "name";
+        }
+
+    }
+
+    public static class Child extends Parent {
+
+        public String getAlias() {
+            return "alias";
+        }
+
     }
 
 }
