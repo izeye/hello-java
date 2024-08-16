5096beedb7f250307bfa7e205bb539f5dc0b2dd4
Johnny Lim <izeye@naver.com>
2024-08-16 20:40:03 +0900

    Add tests for Matcher

--- src/test/java/learningtest/java/util/regex/MatcherTests.java
+++ src/test/java/learningtest/java/util/regex/MatcherTests.java
@@ -2,6 +2,11 @@ package learningtest.java.util.regex;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +19,34 @@ class MatcherTests {
         Pattern pattern = Pattern.compile("abc");
         Matcher matcher = pattern.matcher("abc123abc456abc");
         assertThat(matcher.results().count()).isEqualTo(3);
+    }
+
+    @Test
+    void group() {
+        Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+        Matcher matcher = pattern.matcher("Hello, ${firstName} ${lastName}!");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+
+    @Test
+    void printAllThymeleafVariables() throws IOException {
+        String path = "src/test/resources/thymeleaf.html";
+
+        Set<String> variables = getAllThymeleafVariables(path);
+        variables.forEach(System.out::println);
+    }
+
+    private static Set<String> getAllThymeleafVariables(String path) throws IOException {
+        Set<String> variables = new TreeSet<>();
+        String string = Files.readString(new File(path).toPath());
+        Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            variables.add(matcher.group());
+        }
+        return variables;
     }
 
 }
