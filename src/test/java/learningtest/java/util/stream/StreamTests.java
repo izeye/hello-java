34d380237dcfc57f8ec593d7c804b64563b5e0ef
Johnny Lim <izeye@naver.com>
2025-02-07 14:28:51 +0900

    Add tests for Stream with null element

--- src/test/java/learningtest/java/util/stream/StreamTests.java
+++ src/test/java/learningtest/java/util/stream/StreamTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 /**
  * Tests for {@link Stream}.
@@ -16,6 +17,12 @@ class StreamTests {
     @Test
     void limit() {
         assertThat(Stream.of(1, 2, 3, 4, 5).limit(3)).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void nullElement() {
+        assertThat(Stream.of((String) null)).containsExactly((String) null);
+        assertThatNullPointerException().isThrownBy(() -> Stream.of((String) null).findFirst());
     }
 
 }
