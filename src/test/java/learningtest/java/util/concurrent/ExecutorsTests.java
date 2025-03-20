a4131a38b68ce2ccf1923f5891c09754b9a33226
Johnny Lim <izeye@naver.com>
2025-03-20 22:34:49 +0900

    Add test for creating virtual threads more than carrier threads
    See https://rockthejvm.com/articles/the-ultimate-guide-to-java-virtual-threads

--- src/test/java/learningtest/java/util/concurrent/ExecutorsTests.java
+++ src/test/java/learningtest/java/util/concurrent/ExecutorsTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +23,21 @@ class ExecutorsTests {
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
             assertThat(executorService.submit(() -> Thread.currentThread().getClass().getName()).get())
                     .isEqualTo("java.lang.VirtualThread");
+        }
+    }
+
+    @Test
+    void createVirtualThreadsMoreThanCarrierThreads() {
+        ThreadFactory threadFactory = Thread.ofVirtual().name("my-virtual-thread-", 0).factory();
+        try (ExecutorService executorService = Executors.newThreadPerTaskExecutor(threadFactory)) {
+            IntStream.range(0, Runtime.getRuntime().availableProcessors() + 1).forEach((i) -> executorService.submit(() -> {
+                System.out.println(Thread.currentThread() + ": " + i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }));
         }
     }
 
